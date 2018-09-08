d11cc0aa348275c476410582e76ad9815beee8a0
anandk6758 <anandtiwari12@hotmail.com>
2018-09-08 15:16:29 +0530

    adding hashode, equals and tostring methods to the pojo classes.

--- src/main/java/com/anand/SpringDemo/controllers/BookController.java
+++ src/main/java/com/anand/SpringDemo/controllers/BookController.java
@@ -1,6 +1,7 @@
 package com.anand.SpringDemo.controllers;
 
 import com.anand.SpringDemo.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BookController {
 
+    @Autowired
     private BookRepository bookRepository;
 
-    public BookController(BookRepository bookRepository) {
+    /* public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
-    }
+    } */
 
     @RequestMapping("/books")
     public String getBooks(Model model ){
